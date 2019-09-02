b790bcb26d1bef08ab8b59ef33d646d8c3dfe2cf
Rika Fukamachi <rika.fukamachi@gmail.com>
2019-09-02 12:29:26 -0700

    almost done buldMetadata()

--- Belly_Button_Diversity/static/js/app.js
+++ Belly_Button_Diversity/static/js/app.js
@@ -1,15 +1,64 @@
+/*****************************************************************/
+// @TODO: Complete the following function that builds the metadata panel
+/*****************************************************************/
 function buildMetadata(sample) {
+    
+    //use d3 to select the tbody:
+    let metaDataTable = d3.select('.table tbody');
+    // console.log(metaDataTable);
 
-  // @TODO: Complete the following function that builds the metadata panel
+    //clear the table before refilling if any:
+    metaDataTable.html("");
 
-  // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
 
-    // Use `.html("") to clear any existing metadata
+    //d3.json returns jsonified version of the sample metadata:
+    d3.json(`/metadata/${sample}`)
+      //once retrieved, build the table:
+      .then(function(sampleMetaData) {
+        
+        Object.entries(sampleMetaData).forEach(([key, value]) => {
+          // console.log(key, value);
+          let trow = metaDataTable.append('tr');
+          let tkey = trow.append('td');
+          let tvalue = trow.append('td');
+          tkey.text(key);
+          tvalue.text(value);
+        });
+
+
+      });
+
+
+
+        // // console.log(sampleMetaData);
+
+        // let metaDataArr = Object.entries(sampleMetaData);
+
+        // metaDataArr.forEach(function(factItem){
+        //   console.log('k', factItem[0]);
+        //   console.log('v', factItem[1]);
+        //   console.log('***');
+
+
+        // });
+
+
+        
+
+        // });
+
+
+
+// Use `d3.json` to fetch the metadata for a sample
+  // Use d3 to select the panel with id of `#sample-metadata`
+    // let metaDataPanel = d3.select('#sample-metadata');
+      // Use `.html("") to clear any existing metadata
 
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
+
+
 
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
@@ -18,7 +67,7 @@ function buildMetadata(sample) {
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-
+  // d3.json(`/samples/${sample}`).then(...)
     // @TODO: Build a Bubble Chart using the sample data
 
     // @TODO: Build a Pie Chart
